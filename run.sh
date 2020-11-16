9d1fdb530fed2a486d7e72adcf621bf45d722126
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-16 15:12:13 +0000

    .

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd ~ || return
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y zsh git curl wget glances htop neovim python3-dev\
     python3-pip python3-setuptools gcc valgrind gdb unzip zip tmux p7zip-full\
-    p7zip-rar lynx ripgrep fd-find nodejs npm neofetch screenfetch
+    p7zip-rar lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch screenfetch
 
 # GUI: meld, vscode, etc.
 
