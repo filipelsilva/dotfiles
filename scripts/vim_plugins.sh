503219e63d2cc02f4f413e0774e4961ced7cfe1d
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-18 19:28:16 +0000

    .

--- scripts/vim_plugins.sh
+++ scripts/vim_plugins.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# To solve vim-startify
+touch ~/.viminfo
+
 # Vim VimPlug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
