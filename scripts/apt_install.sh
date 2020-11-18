d8fb9b5557687fa0f7ecc74512c0a56196dd5dec
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-18 14:42:44 +0000

    .

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -4,7 +4,7 @@ cd ~ || return
 
 # Update; upgrade; install stuff
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y zsh git curl wget glances htop neovim python3-dev\
+sudo apt-get install -y zsh git curl wget glances htop neovim golang python3-dev\
     python3-pip python3-setuptools gcc valgrind gdb unzip zip tmux p7zip-full\
     p7zip-rar lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch screenfetch
 
