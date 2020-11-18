d253934e436fe9ce9f0caf9c1a41fba1e6f83c42
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-18 18:55:23 +0000

    .

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Beggining instalation"
+echo "Installing apt packages..."
 ./scripts/apt_install.sh > /dev/null 2>&1
+echo "Installing other programs..."
 ./scripts/other_programs.sh > /dev/null 2>&1
+echo "Installing zsh plugins..."
 ./scripts/zsh_plugins.sh > /dev/null 2>&1
+echo "Installing vim plugin managers..."
 ./scripts/vim_plugins.sh > /dev/null 2>&1
+echo "Linking dotfiles..."
 ./scripts/linker_dotfiles.sh > /dev/null 2>&1
