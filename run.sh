8df50ff020f679ce511f2c09f591aef0ecbfc7b9
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-18 20:34:23 +0000

    .

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-redirect="> /dev/null 2>&1"
-
 echo "#########################"
 echo "# Beggining instalation #"
 echo "#########################"
 echo "# 1. Installing apt packages (this will take a while)..."
-./scripts/apt_install.sh "$redirect"
+sudo apt-get update && sudo apt-get upgrade &&\
+	sudo aptget install vim neovim curl
+#./scripts/apt_install.sh > /dev/null 2>&1
 echo "# 2. Installing other programs (this will also take a while)..."
-./scripts/other_programs.sh "$redirect"
+#./scripts/other_programs.sh > /dev/null 2>&1
 echo "# 3. Installing zsh plugins..."
-./scripts/zsh_plugins.sh "$redirect"
+#./scripts/zsh_plugins.sh > /dev/null 2>&1
 echo "# 4. Installing vim plugin managers..."
-./scripts/vim_plugins.sh "$redirect"
+./scripts/vim_plugins.sh > /dev/null 2>&1
 echo "# 5. Linking dotfiles..."
-./scripts/linker_dotfiles.sh "$redirect"
+./scripts/linker_dotfiles.sh > /dev/null 2>&1
 echo "# 6. Post instalation things..."
-./scripts/post_install.sh
+#./scripts/post_install.sh
