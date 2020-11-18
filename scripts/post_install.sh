b9eed30219e909cb8da358721524c82b9ed65543
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-18 21:14:56 +0000

    .

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# To solve vim-startify
-#touch ~/.viminfo
-
-# For (n)vim's startify
-mkdir -p ~/.vim/files/info
-
 # Neovim Plugins
 nvim -c ":PlugInstall" -c "q" -c "q"
 nvim -c ":so ~/.config/nvim/init.vim" -c "q"
