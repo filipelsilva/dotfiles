b43969c0b19c3d8d4a627cef9a49b587d5d129da
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-18 18:46:13 +0000

    .

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./scripts/apt_install.sh
-./scripts/other_programs.sh
-./scripts/zsh_plugins.sh
-./scripts/vim_plugins.sh
-./scripts/linker_dotfiles.sh
+./scripts/apt_install.sh 2>&1 /dev/null
+./scripts/other_programs.sh 2>&1 /dev/null
+./scripts/zsh_plugins.sh 2>&1 /dev/null
+./scripts/vim_plugins.sh 2>&1 /dev/null
+./scripts/linker_dotfiles.sh 2>&1 /dev/null
