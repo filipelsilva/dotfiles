60dd3dbde4c3c0de5a426452f69bdb384448fd60
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-22 00:25:25 +0000

    .

--- scripts/linker_dotfiles.sh
+++ scripts/linker_dotfiles.sh
@@ -5,9 +5,9 @@ cd ~ || return
 # Dotfiles
 rm ~/.zshrc
 mkdir -p ~/.config/nvim
-ln -s ~/dotfiles/files/init.vim ~/.config/nvim/
-ln -s ~/dotfiles/files/.alacritty.yml ~
-ln -s ~/dotfiles/files/.zshrc ~
-ln -s ~/dotfiles/files/.tmux.conf ~
-ln -s ~/dotfiles/files/.gitconfig ~
-ln -s ~/dotfiles/files/starship.toml ~/.config/
+ln -s ~/dotfiles/files/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/files/alacritty.yml ~/.alacritty.yml
+ln -s ~/dotfiles/files/zshrc ~/.zshrc
+ln -s ~/dotfiles/files/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/files/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/files/starship.toml ~/.config/starship.toml
