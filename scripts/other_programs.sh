0677561dbbcd00970ed1f3b81e98b7a4ec5f1efb
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-23 16:35:25 +0000

    Revert "."
    This reverts commit b6a6b3be67c915b1e5268f23a59be23ca0cdc78f.

--- scripts/other_programs.sh
+++ scripts/other_programs.sh
@@ -6,16 +6,25 @@ cd ~ || return
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
-# Pip programs
-[ "$UID" -eq 0 ] || exec sudo pip3 install tmuxp thefuck bpytop pip-upgrade-outdated
+# Tmuxp
+sudo pip3 install tmuxp
+
+# TheFuck
+sudo pip3 install thefuck
+
+# Bpytop
+sudo pip3 install bpytop
+
+# Pip-upgrade-outdated
+sudo pip3 install pip-upgrade-outdated
 
 # Forgit
 git clone https://github.com/wfxr/forgit ~/.config/forgit
 
 # Gh cli
-[ "$UID" -eq 0 ] || exec sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
-[ "$UID" -eq 0 ] || exec sudo apt-add-repository https://cli.github.com/packages
-[ "$UID" -eq 0 ] || exec sudo apt-get update && sudo apt-get install gh
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt-get update && sudo apt-get install gh
 
 # Rustc / Rustup / Cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
