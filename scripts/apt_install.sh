223e650eeda52b2b2cb85a2ccdc82d723866926a
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-24 11:39:48 +0000

    .

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -10,10 +10,10 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB9
 sudo apt-add-repository https://cli.github.com/packages > /dev/null
 
 # Update; upgrade; install stuff
-sudo apt-get -qq update
-sudo apt-get -qq -y upgrade
+sudo apt-get -qq update > /dev/null
+sudo apt-get -qq -y upgrade > /dev/null
 sudo apt-get install -qq -y zsh git curl wget glances htop neovim golang python3-dev\
     python3-pip python3-setuptools gcc valgrind gdb unzip zip tmux gh p7zip-full\
-    p7zip-rar lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch screenfetch
+    p7zip-rar lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch screenfetch > /dev/null
 
 # GUI: meld, vscode, discord, obs-studio, etc.
