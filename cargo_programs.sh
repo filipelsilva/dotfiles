6a01b58f4c83b9e52037b400d217d9efe47c9968
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-24 10:48:41 +0000

    .

--- cargo_programs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Rustc / Rustup / Cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-
-# Programs
-cargo install hexyl exa bat hyperfine cargo-update
--- pip_programs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Programs
-sudo pip3 install tmuxp thefuck bpytop pip-upgrade-outdated
