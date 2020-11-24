7681f5987a17c87e39e0ae0a04e312ec6b5802f1
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-24 11:25:42 +0000

    .

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -2,14 +2,17 @@
 
 cd ~ || return
 
+# Workaround
+sudo echo "Getting password..."
+
 # Gh cli
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0 > /dev/null
+sudo apt-add-repository https://cli.github.com/packages > /dev/null
 
 # Update; upgrade; install stuff
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install -y zsh git curl wget glances htop neovim golang python3-dev\
+sudo apt-get -qq update
+sudo apt-get -qq -y upgrade
+sudo apt-get install -qq -y zsh git curl wget glances htop neovim golang python3-dev\
     python3-pip python3-setuptools gcc valgrind gdb unzip zip tmux gh p7zip-full\
     p7zip-rar lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch screenfetch
 
