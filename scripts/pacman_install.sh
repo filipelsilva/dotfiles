89f87f0f56078048bb16ff7fac889e4a84ca5413
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-11-30 23:10:10 +0000

    .

--- scripts/pacman_install.sh
+++ scripts/pacman_install.sh
@@ -3,7 +3,6 @@
 # Update; upgrade; install stuff
 sudo pacman -Syu --noconfirm zsh git curl wget glances htop neovim go python \
     python-pip python-setuptools gcc valgrind gdb unzip zip tmux p7zip lynx \
-    ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch p7zip lynx \
     ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch pkg-config \
     make
 
--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -4,5 +4,8 @@
 nvim -c ":PlugInstall" -c "q" -c "q"
 nvim -c ":so ~/.config/nvim/init.vim" -c "q"
 
+# For deoplete
+pip3 install --user pynvim
+
 # Set default shell
 chsh -s "$(command -v zsh)"
