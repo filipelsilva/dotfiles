f3ee5e49542ad1eb7b435ea324b1a3563fc294d5
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-12-03 19:58:01 +0000

    .

--- scripts/other_programs.sh
+++ scripts/other_programs.sh
@@ -10,12 +10,13 @@ git clone https://github.com/wfxr/forgit ~/.config/forgit
 # Bat-extras (requisites)
 GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
 
-npm install --save-dev --save-exact prettier
+(cd ~ && npm install --save-dev --save-exact prettier)
 echo {}> ~/.prettierrc.json
 
 # Bat-extras
 git clone https://github.com/eth-p/bat-extras ~/.bat-extras
-(cd bat-extras && sudo ./build.sh && sudo mv bin/* /usr/local/bin)
+(cd bat-extras && sudo ./build.sh)
+sudo mv ~/.bat-extras/bin/* /usr/local/bin
 
 # Pwndbg + gef + peda (GDB)
 git clone https://github.com/filipelsilva/gdb-peda-pwndbg-gef.git ~/.gdb-peda-pwndbg-gef
