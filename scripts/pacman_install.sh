baf7393df5d0b0ed7f423c4768aabf62d3b8f45c
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-12-04 16:17:03 +0000

    .

--- scripts/pacman_install.sh
+++ scripts/pacman_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Update; upgrade; install stuff
-sudo pacman -Syu --noconfirm zsh git curl wget glances htop neovim go python \
-    python-pip python-setuptools gcc valgrind gdb unzip zip tmux p7zip lynx \
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm zsh git curl wget glances htop neovim go python-pip \
+    python python-setuptools gcc valgrind gdb unzip zip tmux p7zip lynx \
     ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch pkg-config \
-    make entr clang-format
+    make entr clang
 
 # GUI: meld, vscode, discord, obs-studio, etc.
