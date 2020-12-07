088314fb0c80270b88bf7a43f7979335085084d3
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-12-07 10:16:51 +0000

    .

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -5,6 +5,6 @@ sudo apt-get -qq update
 sudo apt-get -qq -y upgrade  
 sudo apt-get install -qq -y zsh git curl wget glances htop neovim golang python3-dev\
     python3-pip python3-setuptools gcc valgrind gdb unzip zip tmux p7zip-full make\
-    p7zip-rar lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch screenfetch\
-    entr clang-format
+    lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch screenfetch entr\
+    clang-format
 # GUI: meld, vscode, discord, obs-studio, etc.
