c7d3a7bd6b68938f97f677de89ae9559a944c5d1
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-12-08 20:17:48 +0000

    .

--- scripts/linker_dotfiles.sh
+++ scripts/linker_dotfiles.sh
@@ -7,5 +7,6 @@ ln -s ~/dotfiles/files/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/files/alacritty.yml ~/.alacritty.yml
 ln -s ~/dotfiles/files/zshrc ~/.zshrc
 ln -s ~/dotfiles/files/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/files/tmate.conf ~/.tmate.conf
 ln -s ~/dotfiles/files/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/files/starship.toml ~/.config/starship.toml
