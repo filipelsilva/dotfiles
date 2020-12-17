b692661062f5df0f3319307a2c338d2be4c7311c
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-12-17 14:42:48 +0000

    .

--- scripts/dnf_install.sh
+++ scripts/dnf_install.sh
@@ -5,6 +5,6 @@ sudo dnf upgrade
 sudo dnf install -y zsh git curl wget glances htop neovim golang python3-devel\
     python3-pip python3-setuptools gcc valgrind gdb unzip zip tmux tmate p7zip\
     make lynx ripgrep fd-find the_silver_searcher nodejs npm neofetch screenfetch\
-    entr clang
+    entr clang util-linux-user openssl-devel
 
 # GUI: meld, vscode, discord, obs-studio, etc.
