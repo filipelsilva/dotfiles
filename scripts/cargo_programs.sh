09ef5d4bbb6726599a1c6f5d5bbe955533ab3118
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2020-12-20 22:57:21 +0000

    .

--- scripts/cargo_programs.sh
+++ scripts/cargo_programs.sh
@@ -5,7 +5,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
 # Programs
-cargo install hexyl exa bat tokei hyperfine cargo-update git-delta topgrade
+cargo install hexyl exa bat tokei tealdeer hyperfine cargo-update git-delta topgrade
 
 git clone https://github.com/mosmeh/indexa ~/indexa
 (cd ~/indexa && cargo install --path .)
