1086fb7e6d27ad8acf46bfbfb0085402e7c7edef
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-01-19 15:41:05 +0000

    .

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -8,7 +8,8 @@ if ( command -v apt-get &> /dev/null ); then
     sudo apt-get install -qq -y zsh git curl wget glances htop neovim\
         python3-dev python3-pip python3-setuptools gcc valgrind gdb unzip zip\
         tmux tmate p7zip-full make lynx ripgrep fd-find silversearcher-ag\
-        nodejs npm neofetch screenfetch entr rlwrap ctags hexyl bat cmake
+        nodejs npm neofetch screenfetch entr rlwrap ctags hexyl bat cmake\
+        shellcheck
 
 elif ( command -v pacman &> /dev/null ); then
 
@@ -16,7 +17,7 @@ elif ( command -v pacman &> /dev/null ); then
     sudo pacman -S --noconfirm zsh git curl wget glances htop neovim python-pip\
         python python-setuptools gcc valgrind gdb unzip zip tmux tmate p7zip\
         lynx ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch\
-        pkg-config make entr rlwrap ctags hexyl bat cmake
+        pkg-config make entr rlwrap ctags hexyl bat cmake shellcheck
     sudo pacman -S --noconfirm exa tokei hyperfine
     ./scripts/yay_programs.sh
 
