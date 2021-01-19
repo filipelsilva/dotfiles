665bf98e7cb7dd5ba9d19ee46de9aecb389a9eeb
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-01-19 15:21:35 +0000

    .

--- scripts/yay_programs.sh
+++ scripts/yay_programs.sh
@@ -4,4 +4,4 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git ~/.yay
 (cd ~/.yay && makepkg -si)
 
-yay -S tealdeer git-delta topgrade
+yay -S --noconfirm tealdeer git-delta topgrade
