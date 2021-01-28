f3b61d2fcae04c30f50df49b53326085619df6d4
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-01-28 03:27:24 +0000

    .

--- scripts/yay_programs.sh
+++ scripts/yay_programs.sh
@@ -4,4 +4,4 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git ~/.yay
 (cd ~/.yay && makepkg -si)
 
-yay -S --noconfirm tealdeer git-delta topgrade
+yay -S --noconfirm tealdeer git-delta topgrade optimus-manager ttf-iosevka ttf-iosevka-term
