fed2ca36b14474f740f6a4e2ee2fb84cfa38b12c
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-02-18 13:39:07 +0000

    .

--- scripts/pip_programs.sh
+++ scripts/pip_programs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Programs
-pip3 install --user --upgrade tmuxp thefuck bpytop pip-upgrade-outdated pynvim
+pip3 install --user --upgrade tmuxp thefuck bpytop pip-upgrade-outdated pynvim bpython
