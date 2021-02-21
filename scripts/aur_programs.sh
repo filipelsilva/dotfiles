d00b073f076d12047f841ea69b5872a2b4ab572b
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-02-21 20:02:45 +0000

    installer for lazygit on ubuntu/arch

--- scripts/aur_programs.sh
+++ scripts/aur_programs.sh
@@ -4,6 +4,6 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/aur.git ~/.aur
 (cd ~/.aur && makepkg -si)
 
-aur -S --noconfirm tealdeer git-delta topgrade bottom dust sd
+aur -S --noconfirm tealdeer git-delta lazygit topgrade bottom dust sd
 # Extras: 
 # aur -S --noconfirm optimus-manager ttf-iosevka ttf-iosevka-term
--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -2,14 +2,14 @@
 
 # Update; upgrade; install stuff
 if ( command -v apt-get &> /dev/null ); then
-
-    sudo apt-get -qq update  
-    sudo apt-get -qq -y upgrade  
+    sudo add-apt-repository ppa:lazygit-team/release
+    sudo apt-get -qq update
+    sudo apt-get -qq -y upgrade
     sudo apt-get install -qq -y zsh git curl wget glances htop neovim\
         python3-dev python3-pip python3-setuptools gcc valgrind gdb unzip zip\
         tmux tmate p7zip-full make lynx ripgrep fd-find silversearcher-ag\
         nodejs npm neofetch screenfetch entr rlwrap ctags hexyl bat cmake\
-        shellcheck
+        shellcheck lazygit
 
 elif ( command -v pacman &> /dev/null ); then
 
