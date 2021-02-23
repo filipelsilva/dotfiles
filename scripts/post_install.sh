304204dc3fd408b69df049fcb4ed509b0a0b6442
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-02-23 20:06:00 +0000

    .

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -2,10 +2,10 @@
 
 # Install neovim plugins
 nvim -c ":PlugInstall" -c "q" -c "q"
-nvim -c ":so ~/.config/nvim/init.vim" -c "q"
+nvim -c ":so ~/.config/nvim/init.vim" -c "q" -c "q"
 
 # Set default shell
 chsh -s "$(command -v zsh)"
 
 # Set completion theme
-fast-theme forest
+zsh -c "source ~/.zshrc && fast-theme forest"
