b496360ca15e3b3491ae01dc72ad3a8d3f2430bc
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-06 17:44:54 +0000

    .

--- scripts/aur_programs.sh
+++ scripts/aur_programs.sh
@@ -4,6 +4,6 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/aur.git ~/.aur
 (cd ~/.aur && makepkg -si)
 
-aur -S --noconfirm tealdeer git-delta lazygit topgrade bottom dust sd
+aur -S --noconfirm tealdeer git-delta lazygit topgrade bottom dust sd neovim-nightly-git
 # Extras: 
 # aur -S --noconfirm optimus-manager ttf-iosevka ttf-iosevka-term
