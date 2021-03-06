6bb6e9d7390f97ee3e6916f0f4d434987f978ef9
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-06 18:38:52 +0000

    .

--- scripts/aur_programs.sh
+++ scripts/aur_programs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S --needed git base-devel
-git clone https://yay.archlinux.org/yay.git ~/.yay
+git clone https://aur.archlinux.org/yay.git ~/.yay
 (cd ~/.yay && makepkg -si)
 
 yay -S --noconfirm tealdeer git-delta lazygit topgrade bottom dust sd neovim-nightly-git
