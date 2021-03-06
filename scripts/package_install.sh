07d35370499d18c7a61dfb4eac583d64e80255e5
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-06 18:06:16 +0000

    .

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -15,7 +15,7 @@ if ( command -v apt-get &> /dev/null ); then
 elif ( command -v pacman &> /dev/null ); then
 
     sudo pacman -Syu --noconfirm
-    sudo pacman -S --noconfirm zsh git curl wget glances htop neovim python-pip\
+    sudo pacman -S --noconfirm zsh git curl wget glances htop python-pip\
         python python-setuptools gcc valgrind gdb unzip zip tmux tmate p7zip\
         lynx ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch\
         pkg-config make entr rlwrap ctags hexyl bat cmake shellcheck go exa\
