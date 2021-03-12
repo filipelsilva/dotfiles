f0ac285fa0409c31445921cf23510d5a61618f8e
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-12 15:49:45 +0000

    removed 7z

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -8,15 +8,15 @@ if ( command -v apt-get &> /dev/null ); then
     sudo apt-get -qq -y upgrade
     sudo apt-get install -qq -y zsh git curl wget glances htop neovim\
         python3-dev python3-pip python3-setuptools gcc valgrind gdb unzip zip\
-        tmux tmate p7zip-full make lynx ripgrep fd-find silversearcher-ag\
-        nodejs npm neofetch screenfetch entr rlwrap exuberant-ctags hexyl bat\
-        cmake shellcheck lazygit golang
+        tmux tmate make lynx ripgrep fd-find silversearcher-ag nodejs npm\
+        neofetch screenfetch entr rlwrap exuberant-ctags hexyl bat cmake\
+        shellcheck lazygit golang
 
 elif ( command -v pacman &> /dev/null ); then
 
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm zsh git curl wget glances htop python-pip\
-        python python-setuptools gcc valgrind gdb unzip zip tmux tmate p7zip\
+        python python-setuptools gcc valgrind gdb unzip zip tmux tmate\
         lynx ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch\
         pkg-config make entr rlwrap ctags hexyl bat cmake shellcheck go exa\
         tokei hyperfine
