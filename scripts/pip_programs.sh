13576ef02d99e7f5ddb8e27ba012dc69f2427e26
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-20 23:47:31 +0000

    .

--- scripts/pip_programs.sh
+++ scripts/pip_programs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Programs
-pip3 install --user --upgrade thefuck bpytop pip-upgrade-outdated pynvim bpython capstone pyelftools
+pip3 install --user --upgrade lookatme lookatme thefuck bpytop pip-upgrade-outdated pynvim bpython capstone pyelftools
