fd06ad77322fc94ddf85bd1880a7a33f83893f53
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-24 16:48:47 +0000

    .

--- scripts/zsh_plugins.sh
+++ scripts/zsh_plugins.sh
@@ -5,9 +5,6 @@ mkdir -p $HOME/.zsh
 # Zoxide
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ajeetdsouza/zoxide/master/install.sh | sudo sh
 
-# Zsh-vim-mode
-git clone https://github.com/softmoth/zsh-vim-mode.git $HOME/.zsh/zsh-vim-mode
-
 # Fast syntax highlighting
 git clone https://github.com/zdharma/fast-syntax-highlighting.git $HOME/.zsh/fast-syntax-highlighting
 
