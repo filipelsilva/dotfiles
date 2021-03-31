72f6e4cba6c55a5994351bbc2291b9a6182bf3c0
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-31 21:38:05 +0100

    lizard analyser

--- scripts/pip_programs.sh
+++ scripts/pip_programs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Programs
-pip3 install --user --upgrade lookatme thefuck bpytop pip-upgrade-outdated pynvim bpython capstone pyelftools
+pip3 install --user --upgrade lizard lookatme thefuck bpytop pip-upgrade-outdated pynvim bpython capstone pyelftools
