a1d3503dcbf0d0f6b865ad4883ce4e09eed3bcf4
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-04-23 14:42:53 +0100

    added pypy

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -10,14 +10,14 @@ if ( command -v apt-get &> /dev/null ); then
         python3-dev python3-pip python3-setuptools gcc valgrind gdb unzip zip\
         tmux make lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch\
         screenfetch entr rlwrap exuberant-ctags hexyl bat cmake shellcheck\
-        golang perf
+        golang perf pypy
 
 elif ( command -v pacman &> /dev/null ); then
 
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm zsh git curl wget glances htop neovim python-pip\
-        python python-setuptools gcc valgrind gdb unzip zip tmux xclip lynx\
-        ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch\
+        python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx\
+        ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch xclip\
         pkg-config make entr rlwrap ctags hexyl bat cmake shellcheck go exa\
         hyperfine perf
     ./scripts/aur_programs.sh
