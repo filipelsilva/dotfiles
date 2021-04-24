c03cc0b0998df459b5dc5042ccd6427017ca8266
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-04-24 01:41:04 +0100

    golang

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -8,13 +8,13 @@ if ( command -v apt-get &> /dev/null ); then
         python3-dev python3-pip python3-setuptools gcc valgrind gdb unzip zip\
         tmux make lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch\
         screenfetch entr rlwrap exuberant-ctags hexyl bat cmake shellcheck\
-        pypy
+        pypy golang
 
 elif ( command -v pacman &> /dev/null ); then
 
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm zsh git curl wget glances htop neovim python-pip\
-        python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx\
+        python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx go\
         ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch xclip\
         pkg-config make entr rlwrap ctags hexyl bat cmake shellcheck exa\
         hyperfine
