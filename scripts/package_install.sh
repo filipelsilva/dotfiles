536eba4b3ad58d24e8e32510b6d5bbc23cc8dd0f
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-04-24 01:36:07 +0100

    lazygit and golang

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -2,24 +2,22 @@
 
 if ( command -v apt-get &> /dev/null ); then
 
-    sudo add-apt-repository ppa:lazygit-team/release
-    sudo add-apt-repository ppa:longsleep/golang-backports
     sudo apt-get -qq update
     sudo apt-get -qq -y upgrade
     sudo apt-get install -qq -y zsh git curl wget glances htop neovim xclip\
         python3-dev python3-pip python3-setuptools gcc valgrind gdb unzip zip\
         tmux make lynx ripgrep fd-find silversearcher-ag nodejs npm neofetch\
         screenfetch entr rlwrap exuberant-ctags hexyl bat cmake shellcheck\
-        golang perf pypy
+        perf pypy
 
 elif ( command -v pacman &> /dev/null ); then
 
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm zsh git curl wget glances htop neovim python-pip\
-        python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx\
+        python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx perf\
         ripgrep fd the_silver_searcher nodejs npm neofetch screenfetch xclip\
-        pkg-config make entr rlwrap ctags hexyl bat cmake shellcheck go exa\
-        hyperfine perf
+        pkg-config make entr rlwrap ctags hexyl bat cmake shellcheck exa\
+        hyperfine
     ./scripts/aur_programs.sh
 
 fi
