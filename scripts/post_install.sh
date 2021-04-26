f70a89c9127b467d26d6ccabf06af6ce71c1624d
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-04-26 13:57:52 +0100

    simpler colors

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -6,14 +6,18 @@ nvim -c ":so $HOME/.config/nvim/init.vim" -c "q" -c "q"
 
 # Set default shell
 chsh -s "$(command -v zsh)"
-
-# Set completion theme
 zsh -c "source $HOME/.zshrc"
 
+# Set completion theme
+zsh -c "fast-theme forest"
+
+# Vi mode in other programs
+echo "set editing-mode vi" >> $HOME/.inputrc
+# Things to do in other files
 echo "Put this in .profile:
 setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
 setxkbmap -option shift:both_capslock"
 
 echo "Put this in .inputrc:
-set editing-mode vi"
+"
