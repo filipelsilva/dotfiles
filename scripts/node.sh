b06eb506004ee7f0d29594bbb44e3fb1825ec12e
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-05-02 13:45:21 +0100

    .

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-# Source nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+curl -s https://webinstall.dev/node | bash
+[ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
 
 # Node
-nvm install node
+webi node
 
 # Packages
 npm install -g diff-so-fancy
