da02cf72139da6acd62ae13fbb4b24e690454f58
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-05-05 12:20:17 +0100

    .

--- scripts/post.sh
+++ scripts/post.sh
@@ -13,11 +13,9 @@ zsh -c "source $HOME/.zshrc"
 
 # Vi mode in other programs
 echo "set editing-mode vi" >> $HOME/.inputrc
+
 # Things to do in other files
 echo "Put this in .profile:
 setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
 setxkbmap -option shift:both_capslock"
-
-echo "Put this in .inputrc:
-"
