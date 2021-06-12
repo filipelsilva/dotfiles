54c4203c9fc71aa68a93c1c18a52eca46c414f53
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-06-12 14:42:08 +0100

    linker mod

--- scripts/linker.sh
+++ scripts/linker.sh
@@ -2,12 +2,13 @@
 
 # Dotfiles
 rm $HOME/.zshrc
+mkdir -p $HOME/.vim/colors
 mkdir -p $HOME/.config/nvim/colors
-ln -s $HOME/dotfiles/files/init.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/files/init.vim $HOME/.vimrc
+ln -s $HOME/dotfiles/files/color.vim $HOME/.vim/colors/color.vim
+ln -s $HOME/dotfiles/files/init.vim $HOME/.config/nvim/init.vim
+ln -s $HOME/dotfiles/files/color.vim $HOME/.config/nvim/colors/color.vim
 ln -s $HOME/dotfiles/files/alacritty.yml $HOME/.alacritty.yml
 ln -s $HOME/dotfiles/files/zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/files/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/files/gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/files/color.vim $HOME/.config/nvim/colors/color.vim
-ln -s $HOME/dotfiles/files/nocolor.vim $HOME/.config/nvim/colors/nocolor.vim
