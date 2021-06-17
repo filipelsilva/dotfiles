13cbe373314ce22045257a81a4a17b8bf768fce5
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-06-17 18:51:57 +0100

    fixed ubuntu

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -7,16 +7,16 @@ if ( command -v apt-get &> /dev/null ); then
     sudo apt-get install -qq -y zsh git curl wget glances htop neovim xclip\
         python3-dev python3-pip python3-setuptools gcc valgrind gdb unzip zip\
         tmux make lynx ripgrep fd-find silversearcher-ag neofetch screenfetch\
-        entr rlwrap exuberant-ctags hexyl bat cmake shellcheck pypy golang tree
+        entr rlwrap exuberant-ctags hexyl bat cmake shellcheck pypy golang tree\
+        libssl-dev
 
 elif ( command -v pacman &> /dev/null ); then
 
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm zsh git curl wget glances htop neovim python-pip\
-        python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx go\
-        ripgrep fd the_silver_searcher neofetch screenfetch xclip pkg-config\
-        make entr rlwrap ctags hexyl bat cmake shellcheck hyperfine tree\
-        onefetch
+        python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx tree\
+        ripgrep fd the_silver_searcher neofetch screenfetch xclip pkg-config go\
+        make entr rlwrap ctags hexyl bat cmake shellcheck hyperfine onefetch
     ./scripts/aur.sh
 
 fi
