0735d4fc42e5c0243c2f265afad4f7ea5e23166a
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-07-03 02:06:02 +0100

    vimrc != init.vim

--- scripts/linker.sh
+++ scripts/linker.sh
@@ -4,8 +4,8 @@
 rm $HOME/.zshrc
 mkdir -p $HOME/.vim/colors
 mkdir -p $HOME/.config/nvim/colors
-ln -s $HOME/dotfiles/files/init.vim $HOME/.vimrc
 ln -s $HOME/dotfiles/files/color.vim $HOME/.vim/colors/color.vim
+ln -s $HOME/dotfiles/files/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/files/init.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/files/color.vim $HOME/.config/nvim/colors/color.vim
 ln -s $HOME/dotfiles/files/alacritty.yml $HOME/.alacritty.yml
