3ab8f6e71204264aecdd1ef0690d23d253313f40
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-07-06 15:28:21 +0100

    major change: vimrcs for vim and nvim, and a basic mode

--- scripts/linker.sh
+++ scripts/linker.sh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.config/nvim/colors
 mkdir .vim-tmp/undo .vim-tmp/swp .vim-tmp/backup
 mkdir .nvim-tmp/undo .nvim-tmp/swp .nvim-tmp/backup
 ln -s $HOME/dotfiles/files/color.vim $HOME/.vim/colors/color.vim
-ln -s $HOME/dotfiles/files/init.vim $HOME/.vimrc
+ln -s $HOME/dotfiles/files/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/files/init.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/files/color.vim $HOME/.config/nvim/colors/color.vim
 ln -s $HOME/dotfiles/files/alacritty.yml $HOME/.alacritty.yml
