b6ac937b58a7beb6476231ade8f4ae4de19a93e9
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-07-08 16:09:32 +0100

    .

---
+++ scripts/monitors.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This is for my current setup, do not run unless you know what you're doing
+
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --primary --mode 2560x1440 --rate 143.97 --pos 0x0 --rotate normal --output eDP-1-1 --mode 1920x1080 --rate 144.00 --pos 2560x360 --rotate normal
