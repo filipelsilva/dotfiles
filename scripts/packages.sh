f73405ef5f902a6c6afb720c22451e7ec0090d7f
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-07-31 23:47:05 +0100

    .

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -5,7 +5,7 @@ sudo pacman -S --noconfirm zsh git curl wget glances htop neovim python-pip\
     python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx tree\
     ripgrep fd the_silver_searcher neofetch screenfetch xclip pkg-config go\
     make entr rlwrap ctags hexyl bat cmake shellcheck hyperfine onefetch\
-    progress npm atool pacman-contrib zsh-completions
+    progress npm atool pacman-contrib zsh-completions ascii
 
 # For manual installation:
 #sudo pacman -S --noconfirm alacritty i3 i3status i3lock dmenu feh arandr\
