e74cc3f2214f85b91dde2869255693c0c0a41def
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-09-01 15:55:42 +0100

    added downgrade for packages

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git $HOME/.yay
 (cd $HOME/.yay && makepkg -si)
 
-yay -S --noconfirm tealdeer sd
+yay -S --noconfirm tealdeer sd downgrade
 
 # Install manually:
 #yay -S --noconfirm ttf-iosevka ttf-iosevka-term ttf-iosevka-ss12 ttf-iosevka-term-ss12
