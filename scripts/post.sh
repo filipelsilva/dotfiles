5913a4297807f30808c2b07f1e4d1ba97075a952
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-09-07 16:22:49 +0100

    scripts changes

--- scripts/post.sh
+++ scripts/post.sh
@@ -10,6 +10,5 @@ echo "set editing-mode vi" >> $HOME/.inputrc
 # Advice for plugins in (Neo)Vim
 echo "Do not forget to run ':PackUpdate' in Vim/Neovim!"
 
-# For my keyboard, US international mode and caps replaced with esc
-#echo "setxkbmap -layout us -variant intl" >> $HOME/.profile
-#echo "setxkbmap -option caps:swapescape" >> $HOME/.profile
+# For my keyboard, US international mode (altgr variant)
+#echo "setxkbmap -layout us -variant altgr-intl" >> $HOME/.profile
