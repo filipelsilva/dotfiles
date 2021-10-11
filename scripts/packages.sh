084f1d7f8b4db9cc0a4c88b530b3302c2316ed25
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-10-11 19:18:27 +0100

    removed dmenu

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -10,7 +10,7 @@ sudo pacman -S --noconfirm zsh git curl wget glances htop neovim python-pip\
 
 # For desktop installation:
 
-# sudo pacman -S --noconfirm alacritty i3 i3status i3lock dmenu feh arandr\
-#     maim pavucontrol xdotool arc-gtk-theme lxappearance thunar autorandr\
-#     thunar-archive-plugin file-roller nomacs okular rofi xss-lock\
+# sudo pacman -S --noconfirm alacritty i3 i3status i3lock maim rofi feh arandr\
+#     nomacs pavucontrol xdotool arc-gtk-theme lxappearance thunar autorandr\
+#     thunar-archive-plugin file-roller okular xss-lock\
 #     ttc-iosevka ttc-iosevka-ss12
