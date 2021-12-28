28b4efcc4104f9663fbe80d9a12d2a5bf7aba741
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-12-28 21:07:39 +0000

    changed packages layout

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
+packages=(
+	gef
+	cht.sh-git
+	tealdeer
+	downgrade
+	vimv-git
+	rr
+	python-gdbgui
+	linux-wifi-hotspot
+)
+
+desktop_packages=()
+
+if [[ $1 = "full" ]]; then
+    packages+=(${desktop_packages[@]})
+fi
+
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git $HOME/.yay
 (cd $HOME/.yay && makepkg -si)
 
-yay -S --noconfirm \
-	gef \
-	cht.sh-git \
-	tealdeer \
-	downgrade \
-	vimv-git \
-	rr \
-	python-gdbgui \
-	linux-wifi-hotspot \
+yay -S --noconfirm ${packages[@]}
