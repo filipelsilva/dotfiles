a62c4b4e0a5c724cc495ed3da9ef4b1216499914
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-06-24 22:37:55 +0100

    updated treesitter options

--- files/init.lua
+++ files/init.lua
@@ -285,11 +285,32 @@ cmp.setup({
 -- Treesitter {{{
 local treesitter = require("nvim-treesitter.configs")
 treesitter.setup({
-	ensure_installed = "all",
+	ensure_installed = {
+		"bash",
+		"c",
+		"cpp",
+		"dockerfile",
+		"go",
+		"go",
+		"html",
+		"java",
+		"javascript",
+		"json",
+		"json",
+		"lua",
+		"make",
+		"markdown",
+		"nix",
+		"python",
+		"rust",
+		"toml",
+		"typescript",
+		"vim"
+	},
 	sync_install = true,
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = true,
+		additional_vim_regex_highlighting = false,
 	},
 	incremental_selection = {
 		enable = false,
