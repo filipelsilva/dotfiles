929c483002a3c7eda6a9a52dea1de67dbadadac7
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-07-06 02:56:59 +0100

    minor changes, last commit message had a typo i believe, sorry for that

--- headless/.config/nvim/init.lua
+++ headless/.config/nvim/init.lua
@@ -4,10 +4,10 @@ vim.g.no_vim_plugins = 1
 -- Default vim config
 vim.cmd("source $HOME/.vimrc")
 
-require("user.cmp")
+require("user.plugins")
 require("user.colorscheme")
+require("user.telescope")
 require("user.comment")
 require("user.lsp")
-require("user.plugins")
-require("user.telescope")
+require("user.cmp")
 require("user.treesitter")
