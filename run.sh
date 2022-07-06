4a0b3e759e627edbb301502b48dd1274ba47391c
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-07-06 13:55:15 +0100

    .

--- run.sh
+++ run.sh
@@ -6,17 +6,17 @@ echo "#########################"
 
 sudo echo "# Getting password..."
 
-echo "# 1. Linker"
+echo "# 1. Pacman"
+./scripts/packages.sh $1
+
+echo "# 2. AUR"
+./scripts/aur.sh $1
+
+echo "# 3. Linker"
 stow headless
 if [[ $1 = "full" ]]; then
 	stow desktop
 fi
-
-echo "# 2. Pacman"
-./scripts/packages.sh $1
-
-echo "# 3. AUR"
-./scripts/aur.sh $1
 
 echo "# 4. Post instalation things..."
 ./scripts/post.sh $1
