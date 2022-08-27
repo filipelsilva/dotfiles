add912717395e30bd4a64d25d1ba652e8f96e7ca
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-08-27 20:20:17 +0100

    Updated installer scripts. Now all of them can use argparse to know the options to use. Updated README.md to reflect that

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ "$(basename $PWD)" != "dotfiles" ]]; then
+	cd "$HOME/dotfiles"
+fi
+
+if [[ -z "$DOTFILES_FULL" ]]; then
+	source scripts/argparse.sh
+	parse_arguments "$@"
+fi
+
 packages=( # {{{
 	# Linux kernel, base packages
 	base
