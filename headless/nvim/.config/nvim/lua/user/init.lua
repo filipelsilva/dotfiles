1cd2a443d0389fa4e844a568ad169769a6c4edfe
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-12-04 12:27:46 +0000

    changed neovim config to use the after folder

--- headless/nvim/.config/nvim/lua/user/init.lua
+++ headless/nvim/.config/nvim/lua/user/init.lua
@@ -1,8 +1,2 @@
 require("user.settings")
 require("user.plugins")
-require("user.colorscheme")
-require("user.telescope")
-require("user.lsp")
-require("user.cmp")
-require("user.treesitter")
-require("user.keymaps")
