e5f1aafa55a77446a58fb8c7fe45ecf149e5cb94
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-06-04 14:46:10 +0100

    changed inversion of sign colors

--- headless/nvim/.config/nvim/after/plugin/colorscheme.lua
+++ headless/nvim/.config/nvim/after/plugin/colorscheme.lua
@@ -4,7 +4,7 @@ vim.o.termguicolors = true
 vim.g.gruvbox_italic = 1
 vim.g.gruvbox_italicize_strings = 1
 vim.g.gruvbox_invert_selection = 0
-vim.g.gruvbox_invert_signs = 1
+vim.g.gruvbox_invert_signs = 0
 vim.g.gruvbox_contrast_dark = "hard"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
