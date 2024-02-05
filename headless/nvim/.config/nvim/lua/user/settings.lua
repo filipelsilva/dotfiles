2db29746386fc343b1ac8a1e53dbd6d0f68b36cd
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-02-05 22:29:09 +0000

    fix: stupid error (for now)

--- headless/nvim/.config/nvim/lua/user/settings.lua
+++ headless/nvim/.config/nvim/lua/user/settings.lua
@@ -4,3 +4,14 @@ vim.opt.eventignore:append({
 	"FocusGained",
 	"FocusLost"
 })
+
+-- Fix for the client offset_encodings error
+-- This is not a good fix, but for now it's the best one
+local notify = vim.notify
+vim.notify = function(msg, ...)
+	if msg:match("warning: multiple different client offset_encodings") then
+		return
+	end
+
+	notify(msg, ...)
+end
