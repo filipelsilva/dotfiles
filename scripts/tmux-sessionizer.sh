2880cb3d57f96ad9651bc7233c066cc621bd7f0d
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-02-13 00:07:55 +0000

    removed echos from tmux-sessionizer

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -23,14 +23,11 @@ else
 fi
 
 if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-	echo tmux new-session -ds "$selected_name" -c "$selected"
 	tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 if [[ -z $TMUX ]]; then
-	echo tmux attach-session -t "$selected_name"
 	tmux attach-session -t "$selected_name"
 else
-	echo tmux switch-client -t "$selected_name"
 	tmux switch-client -t "$selected_name"
 fi
