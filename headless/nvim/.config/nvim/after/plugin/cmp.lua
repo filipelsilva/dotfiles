2f503496a496d22acf87048d771de980004893bc
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-03-05 15:10:24 +0000

    feat: re-added cmp-path, let's see if it keeps working

--- headless/nvim/.config/nvim/after/plugin/cmp.lua
+++ headless/nvim/.config/nvim/after/plugin/cmp.lua
@@ -62,6 +62,7 @@ cmp.setup({
 		{ name = "nvim_lsp" },
 		{ name = "nvim_lua" },
 		{ name = "luasnip" },
+		{ name = "path" },
 		{ name = "buffer" },
 	}),
 })
--- headless/nvim/.config/nvim/lua/user/plugin.lua
+++ headless/nvim/.config/nvim/lua/user/plugin.lua
@@ -103,6 +103,7 @@ lazy.setup({
 			"hrsh7th/cmp-nvim-lua",
 			"saadparwaiz1/cmp_luasnip",
 			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-git"
 		}
 	},
