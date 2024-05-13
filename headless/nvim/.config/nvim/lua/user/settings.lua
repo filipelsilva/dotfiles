9341e5497e3f16238eca1f8ac9b5777e5a2c29d2
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-05-13 13:54:12 +0100

    feat: focus events do not trigger tmux now

--- headless/nvim/.config/nvim/lua/user/settings.lua
+++ headless/nvim/.config/nvim/lua/user/settings.lua
@@ -1,9 +1,3 @@
--- Fix neovim triggering tmux events
--- This is not a good fix, but for now it's the best one
-vim.opt.eventignore:append({
-	"FocusGained",
-	"FocusLost"
-})
 
 -- Fix for the client offset_encodings error
 -- This is not a good fix, but for now it's the best one
