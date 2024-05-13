b6233d925567f8dba272dd0e0a01c73fe1383997
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-05-13 13:55:14 +0100

    chore: minor setup change

--- headless/nvim/.config/nvim/lua/user/plugin.lua
+++ headless/nvim/.config/nvim/lua/user/plugin.lua
@@ -1,6 +1,6 @@
 -- Automatically install lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
