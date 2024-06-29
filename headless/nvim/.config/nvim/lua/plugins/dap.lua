139bd5a4e85e1c38a44abf2f86bd1a6d81ec5393
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-06-29 20:56:57 +0100

    feat: updated dap code

--- headless/nvim/.config/nvim/lua/plugins/dap.lua
+++ headless/nvim/.config/nvim/lua/plugins/dap.lua
@@ -4,44 +4,41 @@ return {
 		"rcarriga/nvim-dap-ui",
 	},
 	config = function()
-		-- Keybinds
 		local ok, dap = pcall(require, "dap")
-		vim.keymap.set('n', '<F5>', dap.continue)
-		vim.keymap.set('n', '<F6>', dap.toggle_breakpoint)
-		vim.keymap.set('n', '<F10>', dap.step_over)
-		vim.keymap.set('n', '<F11>', dap.step_into)
-		vim.keymap.set('n', '<F12>', dap.step_out)
-
-		-- Pretty UI
 		local ok_ui, dapui = pcall(require, "dapui")
-		if not ok or not ok_ui then
+		local ok_vscode, ext_vscode = pcall(require, "dap.ext.vscode")
+		if not ok or not ok_ui or not ok_vscode then
 			return
-		end
-
-		dap.listeners.after.event_initialized["dapui_config"] = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated["dapui_config"] = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited["dapui_config"] = function()
-			dapui.close()
 		end
 
 		-- Load .vscode/launch.json
-		local ok_vscode, ext_vscode = pcall(require, "dap.ext.vscode")
-		if not ok_vscode then
-			return
-		end
-
 		ext_vscode.load_launchjs(nil, {})
 
-		-- GO debug
-		local ok_go, dap_go = pcall(require, "dap-go")
-		if not ok_go then
-			return
-		end
+		-- Keybinds
+		vim.keymap.set("n", "<F1>", dap.continue)
+		vim.keymap.set("n", "<F2>", dap.step_into)
+		vim.keymap.set("n", "<F3>", dap.step_over)
+		vim.keymap.set("n", "<F4>", dap.step_out)
+		vim.keymap.set("n", "<F5>", dap.step_back)
+		vim.keymap.set("n", "<F10>", dap.toggle_breakpoint)
+		vim.keymap.set("n", "<F11>", dap.run_to_cursor)
+		vim.keymap.set("n", "<F12>", dap.restart)
+		vim.keymap.set("n", "<F9>", function()
+			dapui.eval(nil, { enter = true })
+		end)
 
-		dap_go.setup()
+		-- Pretty UI
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 	end
 }
