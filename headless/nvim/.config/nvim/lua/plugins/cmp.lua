ff6257c788a16263df62efba8f2bebb6953a5c43
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-01-04 22:49:05 +0000

    more updates to blink.cmp

--- headless/nvim/.config/nvim/lua/plugins/cmp.lua
+++ headless/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -24,11 +24,17 @@ return {
 		},
 
 		appearance = {
-			nerd_font_variant = "normal",
+			use_nvim_cmp_as_default = true,
 		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "copilot" },
+			default = {
+				"lsp",
+				"path",
+				"snippets",
+				"buffer",
+				"copilot",
+			},
 			cmdline = {},
 			providers = {
 				copilot = {
@@ -36,16 +42,31 @@ return {
 					module = "blink-cmp-copilot",
 					score_offset = 100,
 					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Copilot"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
 				},
 			},
 		},
 
 		completion = {
 			keyword = { range = "full" },
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 500,
+			menu = {
+				draw = {
+					columns = {
+						{ "label" },
+						{ "kind" },
+						{ "label_description", gap = 1 },
+					},
+				},
 			},
+			documentation = { auto_show = true },
 		},
 
 		signature = { enabled = true },
