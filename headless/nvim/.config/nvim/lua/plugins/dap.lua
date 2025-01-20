49ad2f9afaad6856ccda8bee32308d4e576db886
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-01-20 10:41:22 +0000

    some keybind updates

--- headless/nvim/.config/nvim/lua/plugins/dap.lua
+++ headless/nvim/.config/nvim/lua/plugins/dap.lua
@@ -7,8 +7,7 @@ return {
 	config = function()
 		local ok, dap = pcall(require, "dap")
 		local ok_ui, dapui = pcall(require, "dapui")
-		local ok_vscode, ext_vscode = pcall(require, "dap.ext.vscode")
-		if not ok or not ok_ui or not ok_vscode then
+		if not ok or not ok_ui then
 			return
 		end
 
@@ -18,10 +17,10 @@ return {
 		vim.keymap.set("n", "<F3>", dap.step_over)
 		vim.keymap.set("n", "<F4>", dap.step_out)
 		vim.keymap.set("n", "<F5>", dap.step_back)
-		vim.keymap.set("n", "<F10>", dap.toggle_breakpoint)
-		vim.keymap.set("n", "<F11>", dap.run_to_cursor)
-		vim.keymap.set("n", "<F12>", dap.restart)
-		vim.keymap.set("n", "<F9>", function()
+		vim.keymap.set("n", "<F7>", dap.toggle_breakpoint)
+		vim.keymap.set("n", "<F8>", dap.run_to_cursor)
+		vim.keymap.set("n", "<F9>", dap.restart)
+		vim.keymap.set("n", "<F10>", function()
 			dapui.eval(nil, { enter = true })
 		end)
 
