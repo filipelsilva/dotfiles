62caf727618684952630b6df95e540ee566ced7e
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-04-15 23:04:39 +0100

    updated the copilot settings

--- headless/nvim/.config/nvim/lua/plugins/copilot.lua
+++ headless/nvim/.config/nvim/lua/plugins/copilot.lua
@@ -20,13 +20,31 @@ return {
 			},
 		},
 		build = "make tiktoken",
-		opts = {
-			mappings = {
-				reset = {
-					normal = "",
-					insert = "",
+		opts = function()
+			local select = require("CopilotChat.select");
+			return {
+				model = "claude-3.7-sonnet",
+				agent = "copilot",
+				mappings = {
+					reset = {
+						normal = "",
+						insert = "<C-l>",
+					},
 				},
-			},
-		},
-	},
+				context = {
+					"git",
+					"filenames",
+					"buffers",
+					"files",
+					"quickfix",
+				},
+
+				selection = function(source)
+					return select.visual(source) or select.buffer(source)
+				end,
+
+				show_help = false,
+			}
+		end,
+	}
 }
