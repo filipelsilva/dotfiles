9d1d6ac3635875ba8d4168d46b430b4da2c74c86
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-04-16 19:58:30 +0100

    changed to normal copilot

--- headless/nvim/.config/nvim/lua/plugins/cmp.lua
+++ headless/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -2,7 +2,6 @@ return {
 	"saghen/blink.cmp",
 	dependencies = {
 		"rafamadriz/friendly-snippets",
-		"giuxtaposition/blink-cmp-copilot",
 	},
 
 	version = "*",
@@ -14,8 +13,6 @@ return {
 			["<CR>"] = { "select_and_accept", "fallback" },
 			["<C-n>"] = { "select_next", "snippet_forward", "fallback" },
 			["<C-p>"] = { "select_prev", "snippet_backward", "fallback" },
-			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 			["<C-u>"] = { "scroll_documentation_up", "fallback" },
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 			["<C-d>"] = { "scroll_documentation_down", "fallback" },
@@ -37,29 +34,12 @@ return {
 				"path",
 				"snippets",
 				"buffer",
-				"copilot",
-			},
-			providers = {
-				copilot = {
-					name = "copilot",
-					module = "blink-cmp-copilot",
-					score_offset = 100,
-					async = true,
-					transform_items = function(_, items)
-						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-						local kind_idx = #CompletionItemKind + 1
-						CompletionItemKind[kind_idx] = "Copilot"
-						for _, item in ipairs(items) do
-							item.kind = kind_idx
-						end
-						return items
-					end,
-				},
 			},
 		},
 		completion = {
 			keyword = { range = "full" },
 			menu = {
+				border = "none",
 				draw = {
 					columns = {
 						{ "label" },
--- headless/nvim/.config/nvim/lua/plugins/copilot.lua
+++ headless/nvim/.config/nvim/lua/plugins/copilot.lua
@@ -3,25 +3,11 @@ return {
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
 			{ "nvim-lua/plenary.nvim", branch = "master" },
-			{
-				"zbirenbaum/copilot.lua",
-				config = function()
-					local ok_copilot, copilot = pcall(require, "copilot")
-
-					if not ok_copilot then
-						return
-					end
-
-					copilot.setup({
-						suggestion = { enabled = false },
-						panel = { enabled = false },
-					})
-				end,
-			},
+			{ "github/copilot.vim" },
 		},
 		build = "make tiktoken",
 		opts = function()
-			local select = require("CopilotChat.select");
+			local select = require("CopilotChat.select")
 			return {
 				model = "claude-3.7-sonnet",
 				agent = "copilot",
@@ -46,5 +32,5 @@ return {
 				show_help = false,
 			}
 		end,
-	}
+	},
 }
