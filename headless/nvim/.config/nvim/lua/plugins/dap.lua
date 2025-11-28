0bad2b2110bf7ed0092478d4d30a1db45360ab51
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-11-28 15:05:12 +0000

    i'm not using nested tmux

--- headless/nvim/.config/nvim/lua/plugins/dap.lua
+++ headless/nvim/.config/nvim/lua/plugins/dap.lua
@@ -14,15 +14,15 @@ return {
 		end
 
 		-- Keybinds
-		vim.keymap.set("n", "<Leader>1", dap.continue)
-		vim.keymap.set("n", "<Leader>2", dap.step_into)
-		vim.keymap.set("n", "<Leader>3", dap.step_over)
-		vim.keymap.set("n", "<Leader>4", dap.step_out)
-		vim.keymap.set("n", "<Leader>5", dap.step_back)
-		vim.keymap.set("n", "<Leader>7", dap.toggle_breakpoint)
-		vim.keymap.set("n", "<Leader>8", dap.run_to_cursor)
-		vim.keymap.set("n", "<Leader>9", dap.restart)
-		vim.keymap.set("n", "<Leader>0", function()
+		vim.keymap.set("n", "<F1>", dap.continue)
+		vim.keymap.set("n", "<F2>", dap.step_into)
+		vim.keymap.set("n", "<F3>", dap.step_over)
+		vim.keymap.set("n", "<F4>", dap.step_out)
+		vim.keymap.set("n", "<F5>", dap.step_back)
+		vim.keymap.set("n", "<F7>", dap.toggle_breakpoint)
+		vim.keymap.set("n", "<F8>", dap.run_to_cursor)
+		vim.keymap.set("n", "<F9>", dap.restart)
+		vim.keymap.set("n", "<F10>", function()
 			dapui.eval(nil, { enter = true })
 		end)
 
