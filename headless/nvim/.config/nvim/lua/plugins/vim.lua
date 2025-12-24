fd620b850c0a2ae9fdb5325a85e9cee02f01ddeb
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-12-24 14:07:00 +0000

    some improvements to fugitive experience

--- headless/nvim/.config/nvim/lua/plugins/vim.lua
+++ headless/nvim/.config/nvim/lua/plugins/vim.lua
@@ -17,7 +17,21 @@ return {
 	"tpope/vim-abolish",
 
 	-- Git stuff
-	"tpope/vim-fugitive",
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.cmd([[
+				" Add git branch to statusline
+				set statusline=%<%f\ %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%)\ %P
+
+				" Auto-clean fugitive buffers
+				autocmd BufReadPost fugitive://* set bufhidden=delete
+
+				" .. goes to parent on Fugitive tree/blob views
+				autocmd User fugitive if fugitive#buffer().type() =~# '^\%(tree\|blob\)$' | nnoremap <buffer> .. :edit %:h<CR> | endif
+			]])
+		end,
+	},
 
 	-- Undo tree
 	"mbbill/undotree",
